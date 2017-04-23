fb33d2f0125b5b73112667f0aa8387e0fd05c38b
SeeeRGo <seeergo@yahoo.com>
2017-04-23 19:28:14 +0400

    redo about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,7 +7,14 @@ var React = require('react');
 //			);
 //	}
 //});
-var About = (props) => <h3>About Component</h3>
+var About = (props) => {
+	return (
+		<div>
+			<h3>About Component</h3>
+			<p> Welcome to about page</p>
+		</div>
+		);
+}
 			
 
 module.exports = About;

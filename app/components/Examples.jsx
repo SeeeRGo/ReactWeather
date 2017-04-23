61a8ef9718cc5772189ea23402ec6cc35cbcd005
SeeeRGo <seeergo@yahoo.com>
2017-04-23 22:33:57 +0400

    Examples change & style of Main

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
 	return (
 		<div>
-		<h3>Examples</h3>
-		<p>Tetx</p>
+		<h1 className="text-center">Examples</h1>
+		<p>Here are a few example locations to try out:</p>
+		<ol>
+			<li>
+				<Link to='/?location=Togliatty'>Togliatty</Link>
+			</li>
+			<li>
+				<Link to='/?location=Moscow'>Moscow</Link>
+			</li>
+		</ol>
 		</div>
 		);
 }
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,15 +2,43 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 
-var Nav = (props) => {
+var Nav = React.createClass ({
+	onSearch: function(e){
+		e.preventDefault();
+	},
+	render: function(){
 	return(
-		<div>
-			<h2>Nav Bar</h2>
-			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-			<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-			<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="menu">
+					<li className="menu-text">My Weather App</li>
+					<li>
+						<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+					</li>
+					<li>
+						<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+					</li>
+					<li>
+						<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+				<form onSubmit={this.onSearch}>
+					<ul className="menu">
+						<li>
+							<input type="search"/>
+						</li>
+						<li>
+							<input type="submit" className="button" value="Get Weather"/>
+						</li>
+					</ul>
+				</form>
+			</div>
 		</div>
 		);
-}
+	}
+})
 
 module.exports = Nav;
+
